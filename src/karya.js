838b4bd7d5a68a6f7416a083fac8f16a8a9f9972
fauzanhidayats <fauzansh212@gmail.com>
2025-04-14 22:19:23 +0700

    update-link-js

--- src/karya.js
+++ src/karya.js
@@ -1,8 +1,5 @@
 // Import CSS Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// Import JS Bootstrap (termasuk Popper.js untuk fitur-fitur Bootstrap seperti dropdown, modal)
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 // Optional: CSS kamu sendiri
 import "./style.css";
